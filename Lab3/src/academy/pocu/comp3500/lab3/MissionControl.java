9dc64444ec7dba8e82e93c8620a25c2545c92571
Dong Gyu Kim <fwics88@gmail.com>
2021-09-20 10:41:00 +0900

    lab2

--- Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
+++ Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
@@ -7,10 +7,84 @@ public final class MissionControl {
     }
 
     public static int findMaxAltitudeTime(final int[] altitudes) {
+
+        return findMaxAltitudeTimeRecursive(altitudes, 0, altitudes.length - 1);
+    }
+
+    private static int findMaxAltitudeTimeRecursive(final int[] altitudes, final int left, final int right) {
+        final int mid = (left + right) / 2;
+
+        if (mid == 0 || mid == altitudes.length - 1) {
+            return -1;
+        }
+
+        if (altitudes[mid - 1] < altitudes[mid] && altitudes[mid] > altitudes[mid + 1]) {
+            return mid;
+        }
+
+        if (altitudes[mid - 1] < altitudes[mid] && altitudes[mid] < altitudes[mid + 1]) {
+            return findMaxAltitudeTimeRecursive(altitudes, mid + 1, right);
+        }
+
+        if (altitudes[mid - 1] > altitudes[mid] && altitudes[mid] > altitudes[mid + 1]) {
+            return findMaxAltitudeTimeRecursive(altitudes, left, mid - 1);
+        }
+
+        assert (false);
         return -1;
     }
 
     public static ArrayList<Integer> findAltitudeTimes(final int[] altitudes, final int targetAltitude) {
-        return null;
+
+        ArrayList<Integer> outTimes = new ArrayList<Integer>(altitudes.length / 2);
+        findAltitudeTimesRecursive(targetAltitude, altitudes, 0, altitudes.length - 1, outTimes, true, true);
+        return outTimes;
+    }
+
+    private static void findAltitudeTimesRecursive(final int targetAltitude, final int[] altitudes, final int left, final int right, final ArrayList<Integer> outTimes, final boolean isFindLeft, final boolean isFindRight) {
+        final int mid = (left + right) / 2;
+
+        if (altitudes[mid] == targetAltitude) {
+            outTimes.add(mid);
+            return;
+        }
+
+        if (mid == 0 || mid == altitudes.length - 1) {
+            return;
+        }
+
+        if (altitudes[mid - 1] < altitudes[mid] && altitudes[mid] > altitudes[mid + 1]) {
+            if (isFindLeft) {
+                findAltitudeTimesRecursive(targetAltitude, altitudes, left, mid - 1, outTimes, true, false);
+            }
+
+            if (isFindRight) {
+                findAltitudeTimesRecursive(targetAltitude, altitudes, mid + 1, right, outTimes, false, true);
+            }
+        } else if (altitudes[mid - 1] < altitudes[mid] && altitudes[mid] < altitudes[mid + 1]) {
+
+            if (targetAltitude < altitudes[mid]) {
+                if (isFindLeft) {
+                    findAltitudeTimesRecursive(targetAltitude, altitudes, left, mid - 1, outTimes, true, false);
+                }
+            }
+
+            if (isFindRight) {
+                findAltitudeTimesRecursive(targetAltitude, altitudes, mid + 1, right, outTimes, false, true);
+            }
+        } else if (altitudes[mid - 1] > altitudes[mid] && altitudes[mid] > altitudes[mid + 1]) {
+            if (targetAltitude > altitudes[mid]) {
+                if (isFindLeft) {
+                    findAltitudeTimesRecursive(targetAltitude, altitudes, left, mid - 1, outTimes, true, false);
+                }
+            }
+
+            if (isFindRight) {
+                findAltitudeTimesRecursive(targetAltitude, altitudes, mid + 1, right, outTimes, false, true);
+            }
+        }
+
+        assert (false);
+        return;
     }
 }
--- Lab3/src/academy/pocu/comp3500/lab3/app/Program.java
+++ Lab3/src/academy/pocu/comp3500/lab3/app/Program.java
@@ -1,8 +1,34 @@
 package academy.pocu.comp3500.lab3.app;
 
+import academy.pocu.comp3500.lab3.MissionControl;
+
+import java.util.ArrayList;
+
 public class Program {
 
     public static void main(String[] args) {
-	    // write your code here
+        {
+            final int[] altitudes = new int[] { 1, 2, 3, 4, 5, 6, 7, 4, 3, 2 };
+
+            final int maxAltitudeTime = MissionControl.findMaxAltitudeTime(altitudes);
+
+            assert (maxAltitudeTime == 6);
+        }
+
+        {
+            final int[] altitudes = new int[] { 1, 2, 3, 4, 5, 6, 7, 4, 3, 2 };
+
+            ArrayList<Integer> bounds = MissionControl.findAltitudeTimes(altitudes, 2);
+
+            assert (bounds.size() == 2);
+
+            assert (bounds.get(0) == 1);
+            assert (bounds.get(1) == 9);
+
+            bounds = MissionControl.findAltitudeTimes(altitudes, 5);
+
+            assert (bounds.size() == 1);
+            assert (bounds.get(0) == 4);
+        }
     }
 }
