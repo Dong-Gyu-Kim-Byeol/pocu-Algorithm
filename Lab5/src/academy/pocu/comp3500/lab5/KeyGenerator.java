d3429d258600a24da958a758ae3f00f3c1c4670d
Dong Gyu Kim <fwics88@gmail.com>
2021-10-06 05:59:15 +0900

    lab5

--- Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
+++ Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
@@ -3,24 +3,28 @@ package academy.pocu.comp3500.lab5;
 import java.math.BigInteger;
 
 public class KeyGenerator {
-    private static final BigInteger ZERO = BigInteger.valueOf(0);
-    private static final BigInteger ONE = BigInteger.valueOf(1);
-    private static final BigInteger TWO = BigInteger.valueOf(2);
+    private static final BigInteger ZERO = BigInteger.ZERO;
+    private static final BigInteger ONE = BigInteger.ONE;
+    private static final BigInteger TWO = BigInteger.TWO;
     private static final BigInteger THREE = BigInteger.valueOf(3);
     private static final BigInteger FIVE = BigInteger.valueOf(5);
     private static final BigInteger SIX = BigInteger.valueOf(6);
 
     public static boolean isPrime(final BigInteger number) {
-        if (number.compareTo(TWO) == 0 || number.compareTo(THREE) == 0) {
+        if (number.signum() <= 0) {
+            return false;
+        }
+
+        if (number.equals(TWO) || number.equals(THREE)) {
             return true;
         }
 
-        if (number.compareTo(ONE) <= 0 || number.mod(TWO).compareTo(ZERO) == 0 || number.mod(THREE).compareTo(ZERO) == 0) {
+        if (number.equals(ONE) || number.mod(TWO).equals(ZERO) || number.mod(THREE).equals(ZERO)) {
             return false;
         }
 
         for (BigInteger i = FIVE; i.multiply(i).compareTo(number) <= 0; i = i.add(SIX)) {
-            if (number.mod(i).compareTo(ZERO) == 0 || number.mod(i.add(TWO)).compareTo(ZERO) == 0) {
+            if (number.mod(i).equals(ZERO) || number.mod(i.add(TWO)).equals(ZERO)) {
                 return false;
             }
         }
