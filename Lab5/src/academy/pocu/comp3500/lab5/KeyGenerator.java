6d4306eaf68e72da17b6aab829170de3c7183f69
Dong Gyu Kim <fwics88@gmail.com>
2021-10-06 10:29:32 +0900

    lab5

--- Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
+++ Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
@@ -1,52 +1,34 @@
 package academy.pocu.comp3500.lab5;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 public class KeyGenerator {
     private static final BigInteger ZERO = BigInteger.ZERO;
     private static final BigInteger ONE = BigInteger.ONE;
     private static final BigInteger TWO = BigInteger.TWO;
     private static final BigInteger THREE = BigInteger.valueOf(3);
-    private static final BigInteger FIVE = BigInteger.valueOf(5);
-    private static final BigInteger SIX = BigInteger.valueOf(6);
+    private static final BigInteger FOUR = BigInteger.valueOf(4);
+    private static final Random RANDOM = new Random();
+    private static final int TEST_COUNT = 20;
 
     public static boolean isPrime(final BigInteger number) {
-        final byte[] numberBytes = number.toByteArray();
-        final short firstByte = (short) (numberBytes[0] & 0xff);
-        final short lastByte = (short) (numberBytes[numberBytes.length - 1] & 0xff);
-
-        if (numberBytes[0] < 0) {
+        if (number.signum() <= 0 || number.equals(ONE) || number.equals(FOUR)) {
             return false;
         }
 
-        // number == 1
-        if (numberBytes.length == 1 && firstByte == 1) {
-            return false;
-        }
-
-        // number == 2
-        if (numberBytes.length == 1 && firstByte == 2) {
+        if (number.equals(TWO) || number.equals(THREE)) {
             return true;
         }
 
-        // number == 3
-        if (numberBytes.length == 1 && firstByte == 3) {
-            return true;
+        // n - 1 = d * 2 ^ r (r >= 1)
+        BigInteger d = number.subtract(ONE);
+        while (d.mod(TWO).equals(ZERO)) {
+            d = d.divide(TWO);
         }
 
-        // number % 2 == 0
-        if (lastByte % 2 == 0) {
-            return false;
-        }
-
-        // number % 3 == 0
-        if (number.mod(THREE).equals(ZERO)) {
-            return false;
-        }
-
-        final BigInteger numberSqrt = number.sqrt();
-        for (BigInteger i = FIVE; i.compareTo(numberSqrt) <= 0; i = i.add(SIX)) {
-            if (number.mod(i).equals(ZERO) || number.mod(i.add(TWO)).equals(ZERO)) {
+        for (int i = 0; i < TEST_COUNT; i++) {
+            if (isCanPrime(number, d) == false) {
                 return false;
             }
         }
@@ -54,4 +36,44 @@ public class KeyGenerator {
         return true;
     }
 
+    private static boolean isCanPrime(final BigInteger num, BigInteger d) {
+        // Miller–Rabin test
+        // https://www.geeksforgeeks.org/primality-test-set-3-miller-rabin/
+
+        // a: [2, n − 2]
+        final BigInteger upperLimit = num.subtract(FOUR);
+        BigInteger a;
+        do {
+            a = new BigInteger(upperLimit.bitLength(), RANDOM);
+        } while (a.compareTo(upperLimit) > 0);
+        a = a.add(TWO);
+
+        // Compute a^d % n
+        BigInteger x = a.modPow(d, num);
+        final BigInteger numSubOne = num.subtract(ONE);
+
+        if (x.equals(ONE) || x.equals(numSubOne)) {
+            return true;
+        }
+
+        // Keep squaring x while one of the
+        // following doesn't happen
+        // (i) d does not reach n-1
+        // (ii) (x^2) % n is not 1
+        // (iii) (x^2) % n is not n-1
+        while (d.equals(numSubOne) == false) {
+            x = x.modPow(TWO, num);
+            d = d.multiply(TWO);
+
+            if (x.equals(ONE)) {
+                return false;
+            }
+            if (x.equals(numSubOne)) {
+                return true;
+            }
+        }
+
+        // Return composite
+        return false;
+    }
 }
