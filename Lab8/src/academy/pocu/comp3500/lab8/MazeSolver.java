e416384783ca7191f4a8a3bff919d3fd9bb8dac8
Desk\Desk <fwics88@gmail.com>
2021-11-11 16:32:02 +0900

    lab8

--- Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
+++ Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
@@ -7,11 +7,6 @@ import java.util.List;
 
 public final class MazeSolver {
     public static List<Point> findPath(final char[][] maze, final Point start) {
-        return findPathBfs(maze, start);
-//        return findPathDfs(maze, start);
-    }
-
-    public static List<Point> findPathBfs(final char[][] maze, final Point start) {
         if (maze[start.getY()][start.getX()] == 'E') {
             final ArrayList<Point> path = new ArrayList<Point>(1);
             path.add(start);
@@ -22,15 +17,17 @@ public final class MazeSolver {
 
         final CircularQueue<Path> pathBfsQueue = new CircularQueue<Path>(1);
         pathBfsQueue.enqueue(new Path(start));
-        isVisit[start.getY()][start.getX()] = true;
 
         // top left : (0, 0)
         while (pathBfsQueue.size() != 0) {
             final Path nowPath = pathBfsQueue.dequeue();
-
             final Point nowPos = nowPath.getNowPosition();
 
-            assert (isVisit[nowPos.getY()][nowPos.getX()]);
+            if (isVisit[nowPos.getY()][nowPos.getX()]) {
+                continue;
+            }
+
+            isVisit[nowPos.getY()][nowPos.getX()] = true;
 
             switch (maze[nowPos.getY()][nowPos.getX()]) {
                 case 'E':
@@ -50,46 +47,44 @@ public final class MazeSolver {
                 final int RIGHT_MOVE_TYPE = 2;
                 final int LEFT_MOVE_TYPE = 3;
 
-                final int x;
-                final int y;
+                final int nextX;
+                final int nextY;
                 switch (moveType) {
                     case DOWN_MOVE_TYPE:
-                        x = nowPos.getX();
-                        y = nowPos.getY() + 1;
+                        nextX = nowPos.getX();
+                        nextY = nowPos.getY() + 1;
                         break;
                     case UP_MOVE_TYPE:
-                        x = nowPos.getX();
-                        y = nowPos.getY() - 1;
+                        nextX = nowPos.getX();
+                        nextY = nowPos.getY() - 1;
                         break;
                     case LEFT_MOVE_TYPE:
-                        x = nowPos.getX() - 1;
-                        y = nowPos.getY();
+                        nextX = nowPos.getX() - 1;
+                        nextY = nowPos.getY();
                         break;
                     case RIGHT_MOVE_TYPE:
-                        x = nowPos.getX() + 1;
-                        y = nowPos.getY();
+                        nextX = nowPos.getX() + 1;
+                        nextY = nowPos.getY();
                         break;
                     default:
                         throw new IllegalArgumentException("Unknown move type");
                 }
 
-                if (0 > y || y >= maze.length
-                        || 0 > x || x >= maze[0].length) {
+                if (0 > nextY || nextY >= maze.length
+                        || 0 > nextX || nextX >= maze[0].length) {
                     continue;
                 }
 
-                if (maze[y][x] == 'x') {
+                if (maze[nextY][nextX] == 'x') {
                     continue;
                 }
 
-                if (isVisit[y][x]) {
+                if (isVisit[nextY][nextX]) {
                     continue;
                 }
 
 
-                isVisit[y][x] = true;
-
-                final Point nextPos = new Point(x, y);
+                final Point nextPos = new Point(nextX, nextY);
                 final Path nextPath = new Path(nowPath);
                 nextPath.move(nextPos);
                 pathBfsQueue.enqueue(nextPath);
@@ -97,94 +92,5 @@ public final class MazeSolver {
         }
 
         return new ArrayList<Point>(0);
-    }
-
-    public static List<Point> findPathDfs(final char[][] maze, final Point start) {
-        if (maze[start.getY()][start.getX()] == 'E') {
-            final ArrayList<Point> path = new ArrayList<Point>(1);
-            path.add(start);
-            return path;
-        }
-
-        final boolean[][] isVisit = new boolean[maze.length][maze[0].length];
-
-        final Stack<Point> pointOrNullDfsStack = new Stack<>(2);
-
-        final ArrayList<Point> outPath = new ArrayList<>();
-        final Stack<Integer> pathIndex = new Stack<>(2);
-
-        pointOrNullDfsStack.push(start);
-        // top left : (0, 0)
-        while (!pointOrNullDfsStack.isEmpty()) {
-            final Point nowPos = pointOrNullDfsStack.pop();
-            if (nowPos == null) {
-                final int removeIndex = pathIndex.pop();
-                outPath.remove(removeIndex);
-                continue;
-            }
-
-            switch (maze[nowPos.getY()][nowPos.getX()]) {
-                case 'E':
-                    isVisit[nowPos.getY()][nowPos.getX()] = true;
-
-                    pathIndex.push(outPath.size());
-                    outPath.add(nowPos);
-                    return outPath;
-                case ' ':
-                    isVisit[nowPos.getY()][nowPos.getX()] = true;
-
-                    pathIndex.push(outPath.size());
-                    outPath.add(nowPos);
-                    break;
-                case 'x':
-                    assert (false);
-                    continue;
-                default:
-                    throw new IllegalArgumentException("Unknown type");
-            }
-
-            pointOrNullDfsStack.push(null);
-            for (int moveType = 0; moveType < 4; ++moveType) {
-                final int UP_MOVE_TYPE = 0;
-                final int DOWN_MOVE_TYPE = 1;
-                final int RIGHT_MOVE_TYPE = 2;
-                final int LEFT_MOVE_TYPE = 3;
-
-                final Point nextPos;
-                switch (moveType) {
-                    case DOWN_MOVE_TYPE:
-                        nextPos = new Point(nowPos.getX(), nowPos.getY() + 1);
-                        break;
-                    case UP_MOVE_TYPE:
-                        nextPos = new Point(nowPos.getX(), nowPos.getY() - 1);
-                        break;
-                    case LEFT_MOVE_TYPE:
-                        nextPos = new Point(nowPos.getX() - 1, nowPos.getY());
-                        break;
-                    case RIGHT_MOVE_TYPE:
-                        nextPos = new Point(nowPos.getX() + 1, nowPos.getY());
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Unknown move type");
-                }
-
-                if (0 > nextPos.getY() || nextPos.getY() >= maze.length
-                        || 0 > nextPos.getX() || nextPos.getX() >= maze[0].length) {
-                    continue;
-                }
-
-                if (isVisit[nextPos.getY()][nextPos.getX()]) {
-                    continue;
-                }
-
-                if (maze[nextPos.getY()][nextPos.getX()] == 'x') {
-                    continue;
-                }
-
-                pointOrNullDfsStack.push(nextPos);
-            }
-        }
-
-        return outPath;
     }
 }
