7dd41fe1596c535e7dbb628b2dcf88797596323c
Desk\Desk <fwics88@gmail.com>
2021-11-11 21:04:31 +0900

    no message

--- Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
+++ Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
@@ -3,11 +3,13 @@ package academy.pocu.comp3500.lab8;
 import academy.pocu.comp3500.lab8.maze.Point;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public final class MazeSolver {
     public static List<Point> findPath(final char[][] maze, final Point start) {
-        return myGenericFindPath(maze, start);
+//        return myGenericFindPath(maze, start);
+        return javaGenericFindPath(maze, start);
     }
 
 
@@ -105,6 +107,100 @@ public final class MazeSolver {
         final ArrayList<Point> outPath = new ArrayList<>(reverse.size());
         while (!reverse.isEmpty()) {
             outPath.add(reverse.pop());
+        }
+
+        return outPath;
+    }
+
+
+    public static List<Point> javaGenericFindPath(final char[][] maze, final Point start) {
+        Node lastNodeOrNull = null;
+
+        final boolean[][] isVisit = new boolean[maze.length][maze[0].length];
+
+        final LinkedList<Node> bfs = new LinkedList<Node>();
+        bfs.add(new Node(start, null));
+        isVisit[start.getY()][start.getX()] = true;
+
+        // top left : (0, 0)
+        force_break:
+        while (bfs.size() != 0) {
+            final Node nowNode = bfs.poll();
+
+            final Point nowPos = nowNode.getNowPos();
+
+            switch (maze[nowPos.getY()][nowPos.getX()]) {
+                case 'E':
+                    lastNodeOrNull = nowNode;
+                    break force_break;
+                case ' ':
+                    break;
+                case 'x':
+                    continue;
+                default:
+                    throw new IllegalArgumentException("Unknown type");
+            }
+
+
+            for (int moveType = 0; moveType < 4; ++moveType) {
+                final int UP_MOVE_TYPE = 0;
+                final int DOWN_MOVE_TYPE = 1;
+                final int RIGHT_MOVE_TYPE = 2;
+                final int LEFT_MOVE_TYPE = 3;
+
+                final int nextX;
+                final int nextY;
+                switch (moveType) {
+                    case DOWN_MOVE_TYPE:
+                        nextX = nowPos.getX();
+                        nextY = nowPos.getY() + 1;
+                        break;
+                    case UP_MOVE_TYPE:
+                        nextX = nowPos.getX();
+                        nextY = nowPos.getY() - 1;
+                        break;
+                    case LEFT_MOVE_TYPE:
+                        nextX = nowPos.getX() - 1;
+                        nextY = nowPos.getY();
+                        break;
+                    case RIGHT_MOVE_TYPE:
+                        nextX = nowPos.getX() + 1;
+                        nextY = nowPos.getY();
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown move type");
+                }
+
+                if (0 > nextY || nextY >= maze.length
+                        || 0 > nextX || nextX >= maze[0].length) {
+                    continue;
+                }
+
+                if (isVisit[nextY][nextX]) {
+                    continue;
+                }
+
+                isVisit[nextY][nextX] = true;
+
+
+                final Point nextPos = new Point(nextX, nextY);
+                final Node nextNowAndPrePosition = new Node(nextPos, nowNode);
+                bfs.add(nextNowAndPrePosition);
+            }
+        }
+
+        final LinkedList<Point> outPath = new LinkedList<Point>();
+
+        if (lastNodeOrNull != null) {
+            Node nowAndPrePosition = lastNodeOrNull;
+            outPath.addFirst(nowAndPrePosition.getNowPos());
+
+            while (nowAndPrePosition.getPrePosOrNull() != null) {
+                final Point prePos = nowAndPrePosition.getPrePosOrNull().getNowPos();
+                outPath.addFirst(prePos);
+
+                nowAndPrePosition = nowAndPrePosition.getPrePosOrNull();
+            }
         }
 
         return outPath;
